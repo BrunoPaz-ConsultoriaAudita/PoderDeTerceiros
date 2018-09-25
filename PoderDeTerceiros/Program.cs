e2e7812dbf3df3d023dd59109b25301411852e1e
Bruno <brunopaz@auditaconsultoria.com.br>
2018-09-25 13:25:54 -0300

    Implementada o tratamento para coletar somente as linhas com produtos

--- PoderDeTerceiros/Program.cs
+++ PoderDeTerceiros/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace PoderDeTerceiros
 	{
 		static void Main(string[] args)
 		{
+			string local = @"C:\Users\TI2\Desktop\Bruno Paz\PoderDeTerceiros\PoderDeTerceiros\PoderDeTerceiros\bin\Debug\batata.txt";
+			StreamReader reader = new StreamReader(System.AppDomain.CurrentDomain.BaseDirectory + "in.txt");
+			StreamWriter writer = new StreamWriter(local);
+			int x = 1;
+			string line;
+			List<string> listaLine = new List<string>();
+			while ((line = reader.ReadLine()) != null)
+			{
+				if(line != "")
+				{
+					listaLine = line.Substring(0, 32).Split(' ').ToList<string>();
+					int val;
+					if (int.TryParse(line.Substring(0, 1), out val))
+					{
+						Console.WriteLine("ok");
+					}
+				}
+				
+
+					
+				//
+			}
+			
+			Console.ReadLine();
 		}
 	}
+
 }
---
+++ PoderDeTerceiros/registro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoderDeTerceiros
+{
+	class registro
+	{
+		public int id { get; set; }
+		public int est { get; set; }
+		public int emit { get; set; }
+		public string item { get; set; }
+		public DateTime dtMovto { get; set; }
+		public int seq { get; set; }
+		public int ser { get; set; }
+		public string num { get; set; }
+		public string natur { get; set; }
+		public double qtd { get; set; }
+		public double saldo { get; set; }
+		public double valor { get; set; }
+		public string mens { get; set; }
+
+		public override string ToString()
+		{
+			string str = est + "|" + emit + "|" + item + "|" + dtMovto + "|" + seq + "|" + ser + "|" + num + "|" + natur + "|" + qtd + "|" + valor + "|" + mens;
+			return str;
+		}
+	}
+}
